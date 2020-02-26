b7e64ede1e05fe30d58074f3a5f20be6a6a2c013
jaeseokim <devjaeseo@gmail.com>
2020-02-26 20:29:08 +0900

    2/26 - #2.8 finish

--- app.js
+++ app.js
@@ -4,24 +4,32 @@ import morgan from "morgan"; //web logger
 import helmet from "helmet"; //web secure engin
 import bodyParser from "body-parser"; //for parsing body ex> POST, GET
 import cookieParser from "cookie-parser" //for parsing cookie 
-
+//router module
+import {
+  userRouter
+} from "./router";
+//express const app
 const app = express();
-const PORT = 3000;
-const handleListening = () =>
-  console.log(`listening on : http://localhost:${PORT}`);
 
-const handleHome = (req, res) => res.send("hello");
+const handleHome = (req, res) => res.send("home index");
 
-const handleProfile = (req, res) => res.send("You are on my profile");
+const handleProfile = (req, res) => res.send("home profile");
 
+//set midleware
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(helmet()); //secure
+app.use(morgan("dev")); //rog
 
+//router
 app.get('/', handleHome);
 
 app.get('/profile', handleProfile);
 
-app.listen(PORT, handleListening);
+// user router에게 전달 midleware => userRouter
+app.use('/user', userRouter);
+
+export default app;
