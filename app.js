ba4f1bebce380a9d1484299a756e1abd5ed6e4c1
jaeseokim <devjaeseo@gmail.com>
2020-02-26 21:18:59 +0900

    2/26 - #2.9 finish

--- app.js
+++ app.js
@@ -5,15 +5,11 @@ import helmet from "helmet"; //web secure engin
 import bodyParser from "body-parser"; //for parsing body ex> POST, GET
 import cookieParser from "cookie-parser" //for parsing cookie 
 //router module
-import {
-  userRouter
-} from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globaloRouter from "./routers/globalRouter";
 //express const app
 const app = express();
-
-const handleHome = (req, res) => res.send("home index");
-
-const handleProfile = (req, res) => res.send("home profile");
 
 //set midleware
 app.use(cookieParser());
@@ -25,11 +21,8 @@ app.use(helmet()); //secure
 app.use(morgan("dev")); //rog
 
 //router
-app.get('/', handleHome);
-
-app.get('/profile', handleProfile);
-
-// user router에게 전달 midleware => userRouter
+app.use('/', globaloRouter);
 app.use('/user', userRouter);
+app.use('/video', videoRouter);
 
 export default app;
