bbdb98223904123e259d82b06ac888c5a3fa7c98
jaeseokim <devjaeseo@gmail.com>
2020-02-26 19:54:48 +0900

    2/26 - #2.7 finish

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # webtube 
 
 Cloning Youtube with Vanilla and NodeJS
-
-## Using...
-
-1. node.js
-2. express engin
-3. babel/node (preset-env)
--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-import express from "express";
+import express from "express"; //web server engin
+//express middleware 
+import morgan from "morgan"; //web logger
+import helmet from "helmet"; //web secure engin
+import bodyParser from "body-parser"; //for parsing body ex> POST, GET
+import cookieParser from "cookie-parser" //for parsing cookie 
+
 const app = express();
-
 const PORT = 3000;
-
-
 const handleListening = () =>
-  console.log('Listening on : http://localhost:${PORT}');
+  console.log(`listening on : http://localhost:${PORT}`);
 
-const middleware = (req, res, next) => {
-  console.log(req.url);
-  next();
-};
-
-const handleHome = (req,res) => res.send("hello");
+const handleHome = (req, res) => res.send("hello");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-app.use(middleware);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get('/', handleHome);
 
-app.get('/profile',handleProfile);
+app.get('/profile', handleProfile);
 
 app.listen(PORT, handleListening);
