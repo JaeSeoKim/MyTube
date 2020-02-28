3ff40548be729d8211312511a2abac09be20dacd
jaeseokim <48559454+JaeSeoKim@users.noreply.github.com>
2020-02-28 14:18:04 +0900

    2/28 - #2.20 finish

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -11,8 +11,8 @@ const videoRouter = express.Router();
 
 videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.video_detail, video_detail);
 videoRouter.get(routes.video_edit, video_edit);
 videoRouter.get(routes.video_delete, video_delete);
+videoRouter.get(routes.video_detail, video_detail);
 
 export default videoRouter;
