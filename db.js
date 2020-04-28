1fcdcf5b098a63e8299e11bbf67bd4b928bf53f8
jaeseokim <devjaeseo@gmail.com>
2020-04-28 10:05:55 +0900

    4/28 #3.2 finish

--- db.js
+++ db.js
@@ -1,28 +1,18 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect("mongodb://localhost:27017/wetube", {
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("✔ DB Connection Succes!");
-const handleClose = error => console.log(`❌ DB Connection Fail : ${error}`);
+const handleOpen = () => console.log("✅ DB Connection Succes!");
+const handleClose = error => console.log(`❎ DB Connection Fail : ${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleClose);
-
-// {
-//   id: 1234,
-//   title: "Video Title - test!",
-//   description: "this video is ~~~",
-//   views: 24,
-//   video_file:
-//     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-//   creator: {
-//     id: 9999,
-//     name: "김아무개",
-//     email: "asdas@nasdc.com"
-//   }
-// }
